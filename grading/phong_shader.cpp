26f3de478dc3a851f8bce123959072ba9ae90079
jovillacorta <jvill055@ucr.edu>
2020-02-10 02:58:05 -0800

    added reflective shading

--- grading/phong_shader.cpp
+++ grading/phong_shader.cpp
@@ -21,45 +21,46 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
     for (int i =0; i < world.lights.size(); i++){ 
     //first find if in shadow
     	vec3 lvec = world.lights[i]->position - intersection_point;
-    	Ray sRay(intersection_point,lvec); //cast a shadow ray from intersect towards the light
-
-    	Hit shadowObj = world.Closest_Intersection(sRay); //check: does the shadow ray hit anything?
-
-    if (!shadowObj.object || lvec.magnitude() < shadowObj.dist){
-    //for each light find their diffuse and specular intensities
-
-    	vec3 Id;
-    	vec3 Is;
-
-    	// Id = R*L*max(l*n) 
-    	// l = light, n = normal
-    	// l = light pos - intersect
-    	//USE NORMALIZED VECTORS for max(l*n)!!!
     	
+    	Ray sRay(intersection_point,lvec); //create a shadow ray from intersect towards the light
+		Hit shadowHit = world.Closest_Intersection(sRay); //check: does the shadow ray hit anything?
 
-    	double dotDiffuse = dot(normal, lvec.normalized());
-    	double maxDiffuse = fmax(0.0, dotDiffuse);
+	    if (!shadowHit.object || lvec.magnitude() < shadowHit.dist || !world.enable_shadows){ 
+	    //if object is not in shadow of another object, or if intersected object is behind light, 
+	    //then for each light find their diffuse and specular intensities
+
+	    	vec3 Id;
+	    	vec3 Is;
+
+	    	// Id = R*L*max(l*n) 
+	    	// l = light, n = normal
+	    	// l = light pos - intersect
+	    	//USE NORMALIZED VECTORS for max(l*n)!!!
+	    	
+
+	    	double dotDiffuse = dot(normal, lvec.normalized());
+	    	double maxDiffuse = fmax(0.0, dotDiffuse);
 
 
-    	Id = color_diffuse * world.lights[i]->Emitted_Light(lvec) * maxDiffuse;
-    	sum_Id += Id;
+	    	Id = color_diffuse * world.lights[i]->Emitted_Light(lvec) * maxDiffuse;
+	    	sum_Id += Id;
 
 
-    	// Is = R*L*max(v*r)^e  *note NORMALIZED 
-    	// r = reflected, v = view
-    	// reflected = -l + 2(l*n)n
-    	// view = ray end - intersect 
+	    	// Is = R*L*max(v*r)^e  *note NORMALIZED 
+	    	// r = reflected, v = view
+	    	// reflected = -l + 2(l*n)n
+	    	// view = ray end - intersect 
 
-    	vec3 vvec = ray.endpoint - intersection_point;
-    	vec3 rvec = -lvec + 2 * dot(lvec,normal) * normal;
+	    	vec3 vvec = ray.endpoint - intersection_point;
+	    	vec3 rvec = -lvec + 2 * dot(lvec,normal) * normal;
 
-    	double dotSpecular = dot(rvec.normalized(), vvec.normalized());
-    	double maxSpecular = fmax(0.0, dotSpecular);
-    	double expSpecular = pow(maxSpecular, specular_power);
+	    	double dotSpecular = dot(rvec.normalized(), vvec.normalized());
+	    	double maxSpecular = fmax(0.0, dotSpecular);
+	    	double expSpecular = pow(maxSpecular, specular_power);
 
-    	Is = color_specular * world.lights[i]->Emitted_Light(lvec) * expSpecular;
-    	sum_Is += Is; 
-    }
+	    	Is = color_specular * world.lights[i]->Emitted_Light(lvec) * expSpecular;
+	    	sum_Is += Is; 
+	    }
 	}
 
 	color += sum_Id + sum_Is;  
--- grading/reflective_shader.cpp
+++ grading/reflective_shader.cpp
@@ -4,9 +4,25 @@
 
 vec3 Reflective_Shader::
 Shade_Surface(const Ray& ray,const vec3& intersection_point,
-    const vec3& normal,int recursion_depth) const
-{
+    const vec3& normal,int recursion_depth) const {
+
     vec3 color;
-    TODO; // determine the color
-    return color;
+
+    color = shader->Shade_Surface(ray, intersection_point, normal, recursion_depth);
+    // reflected = d-2(d-n)n;
+
+
+    if (recursion_depth < world.recursion_depth_limit){ // if within recursion limit, calculate reflected color
+
+		vec3 d = ray.direction;
+		Ray reflected;
+		reflected.endpoint = intersection_point;
+		reflected.direction = d - 2 * dot(d,normal) * normal;
+		
+		return ((1 - reflectivity) * color + reflectivity * world.Cast_Ray(reflected, recursion_depth+1));
+	}
+    
+
+    return (1 - reflectivity) * color; // reach recursion limit = return color without a reflected ray casted
 }
+
